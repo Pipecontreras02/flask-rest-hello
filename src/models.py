aef13fd126f36a23607c4a0c63f707e3554cf05e
Pipecontreras02 <pipecontreras2@gmail.com>
2022-02-17 00:16:43 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -8,6 +8,11 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    def __init__(self, email, password, is_active):
+        self.email = email
+        self.password = password
+        self.is_active = is_active
+        
     def __repr__(self):
         return '<User %r>' % self.username
 
